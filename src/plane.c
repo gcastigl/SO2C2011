6f26d4203bfc2da4a489d57717e63aac581d4d63
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-14 16:06:48 -0300

    plane is now capable of reading the responses from the airline

--- src/plane.c
+++ src/plane.c
@@ -11,11 +11,17 @@ Plane* createPlane(int id) {
 
 void planeProcess(Plane* plane, int* wrPipe, int** rdPipe) {
 	int count;
+	char buf[25];
 	close(wrPipe[READ]);
+	close(rdPipe[WRITE]);
 	for (count = 0; count < 2; count++) {
 		char * msj = "This is plane xx... over!\n";
 		write(wrPipe[WRITE], msj, 20);
 		sleep(getpid() % 4); // random sleep...
+		
+		if (read(rdPipe[READ], buf, 24) > 0) {
+			printf("Message from airline -- %s\n", buf);
+		}
 	}
 	write(wrPipe[WRITE], "I'm out!!", 10);
 	exit(0);	
