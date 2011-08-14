3e4c8639cbac8bbdc5601ff07a11a7b50847c781
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-14 19:54:30 -0300

    implementation of plane strategy.

--- src/plane.c
+++ src/plane.c
@@ -1,10 +1,14 @@
 #include "../include/plane.h"
 
+void updateState(Map* map, Plane* plane);
+int findBestCity(Map* map, Plane* plane);
+int hasEnoughItems(Plane* plane, City* city);
+
 Plane* createPlane(int id) {
 	Plane* plane = (Plane*) malloc(sizeof(Plane));
 	plane->id = id;
 	plane->distanceLeft = -1;
-	plane->targetId = -1;
+	plane->targetId = NO_TARGET;
 	plane->supplies = NULL;
 	return plane;
 }
@@ -24,5 +28,37 @@ void planeProcess(Plane* plane, int* wrPipe, int* rdPipe) {
 	}
 	write(wrPipe[WRITE], "0", 1);
 	exit(0);	
+void updateState(Map* map, Plane* plane) {
+	plane->distanceLeft--;
+	if (plane->distanceLeft == 0) {
+		int newTarget = findBestCity(map, plane);
+		if (newTarget != NO_TARGET) {
+			plane->distanceLeft = map->distances[plane->targetId][newTarget]; 	
+			// Distance from currentTargetId to newTaget
+			plane->targetId = newTarget;
+		}
+	}
 }
+
+int findBestCity(Map* map, Plane* plane) {
+	int i;
+	for (i = 0; i < map->citiesCount; i++) {
+		City * currCity = &(map->cities[i]);
+		if (hasEnoughItems(plane, currCity)) {
+			return 	currCity->id;
+		}
+	}
+	return NO_TARGET;
+}
+
+int hasEnoughItems(Plane* plane, City* city) {
+	return TRUE;
+	int i;
+
+	for (i = 0; i < city->needsCount; i++) {
+		//Item * item = &(city->needs[i]);
+		//TODO: verificar cantidad de recursos
+	}
+}
+
 
