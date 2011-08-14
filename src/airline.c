08c59ce9993725f0560c56e32d54a20cd5874451
Juan Pablo Orsay <jporsay@gmail.com>
2011-08-14 18:36:11 -0300

    corrected select

--- src/airline.c
+++ src/airline.c
@@ -38,8 +38,7 @@ void airlineProcess(Airline* airline) {
 	}
 	// Call to select with no timeout, it will block until an event occurs
 	fd_set readCpy = masterRdFd;
-	fd_set writeCpy = masterWrFd;
-	while (select(airline->planeCount + 1, &readCpy, &writeCpy, NULL, NULL) > 0) {
+	while (select(rdPipes[airline->planeCount - 1][READ] + 1, &readCpy, NULL, NULL, NULL) > 0) {
 		for (i = 0; i < airline->planeCount; i++) {
 			if (FD_ISSET(rdPipes[i][READ], &readCpy)) {
 				if (read(rdPipes[i][READ], buf, MSJ_SIZE) > 0) {
