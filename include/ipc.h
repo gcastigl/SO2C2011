cbe8be269249371f87f97395045b59119f9b6a78
Juan Pablo Orsay <jporsay@gmail.com>
2011-08-17 19:53:55 -0300

    Some minor improvements to ipc.h

--- include/ipc.h
+++ include/ipc.h
@@ -3,13 +3,27 @@
 
 #include "common.h"
 
+#define PACKAGE_SIZE sizeof(ipcPackage)
+#define DATA_SIZE 2048
+
+#define READ 0
+#define WRITE 1
+
 typedef enum {
     CREATE,
 	DESTROY,
 	MESSAGE
 } packageType;
 
-int ipcSetup(int childs);
+typedef struct {
+	int id;
+	char data[DATA_SIZE];
+} ipcPackage;
+
+int childIndex;
+
+int ipcSetup(int ammount);
+int ipcSetupChild(int index);
 int ipcClean();
 ipcPackage getData(int childIndex);
 void sendData(int childIndex, ipcPackage data);
