88e0dfdeb21e97cefbb454eb6cfcc81e2741e540
Juan Pablo Orsay <jporsay@gmail.com>
2011-08-17 19:55:22 -0300

    minor improvements to main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include "../include/main.h"
 
 int main() {
-	Airline* airline = createAirline(12345678, 3);
-	airlineProcess(airline);
+    int numAirlines = 1;
+    int i;
+    for (i = 0; i < numAirlines; i++) {
+        switch(fork()) {
+            case 0:
+                airlineStart(createAirline(12345678, 3));
+                break;
+
+            case ERROR:
+                fatal("Fork Error");
+                break;
+        }
+    }
 	return 0;
 }
 
