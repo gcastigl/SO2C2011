9cdb7bae4fc7a687b4108b7d98f47f0a366c3358
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-17 23:08:50 -0300

    created mathUtils

---
+++ include/mathUtil.h
@@ -0,0 +1,6 @@
+#ifndef _MATHUTIL_
+#define _MATHUTIL_
+
+int min(int n1, int n2);
+
+#endif
---
+++ src/mathUtils.c
@@ -0,0 +1,5 @@
+#include "../include/mathUtil.h"
+
+int min(int n1, int n2) {
+	return n1 < n2 ? n1 : n2;
+}
