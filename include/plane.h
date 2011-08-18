9af2e96f81f94257ccc7ce8fcd45e2501f700de0
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-17 22:04:21 -0300

    added sone new fields to Plane structure. Re-declared createPlane method

--- include/plane.h
+++ include/plane.h
@@ -5,16 +5,21 @@
 #include "map.h"
 
 #define NO_TARGET	-1
+#define SLEEP_TIME	1
 
 typedef struct {
 	int id;
-	int targetId;
-	int distanceLeft;
+	int originCityIndex;
+	int destinationCityIndex;
+	int distanceToDestination;
 	Item* supplies;
+	int suppliesSize;
+	Map* map;
 } Plane;
 
-Plane* createPlane(int id);
+Plane* createPlane(Map* map, int id, int initialCityIndex, Item* supplies, int suppliesSize);
 
 void planeProcess(Plane* plane, int* wrPipe, int* rdPipe);
 
 #endif
+
