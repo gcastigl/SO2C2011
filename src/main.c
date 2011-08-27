18220d3307bf3836c95ab7b5e8320e7bd06ca9d9
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-27 14:10:49 -0300

    airline now recieves a random created map from main

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 int main() {
     int numAirlines = 1;
     int i;
+    Map* map = createRandomMap();
     for (i = 0; i < numAirlines; i++) {
         switch(fork()) {
             case 0:
                 printf("Creating airline...\n");
-                Airline* airline = createAirline(12345678, NULL, 2);
+                Airline* airline = createAirline(12345678, map, 2);
                 airlineStart(airline);
                 break;
-
             case ERROR:
                 fatal("Fork Error");
                 break;
