18832ee1bd7a8a547938cb8b2cb6e31fbc58ff62
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-27 13:48:59 -0300

    randomInt function added to MathUtils

--- include/mathUtil.h
+++ include/mathUtil.h
@@ -1,6 +1,10 @@
 #ifndef _MATHUTIL_
 #define _MATHUTIL_
 
+#include "common.h"
+
 int min(int n1, int n2);
+
+int randomInt(int min, int max);
 
 #endif
--- src/mathUtils.c
+++ src/mathUtils.c
@@ -3,3 +3,8 @@
 int min(int n1, int n2) {
 	return n1 < n2 ? n1 : n2;
 }
+
+int randomInt(int min, int max) {
+	int mod = max - min + 1;
+	return (rand() % mod) + min;
+}
