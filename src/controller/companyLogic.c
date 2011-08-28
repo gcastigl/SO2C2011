9884cf08ad58308e7ca8ce8766a409a4aa95de40
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-28 19:44:02 -0300

    made some changes to ocmpanyLogic.c

--- src/controller/companyLogic.c
+++ src/controller/companyLogic.c
@@ -4,6 +4,11 @@ void wakeUpPlanes(Company* company, int semId);
 
 void waitUntilPlanesReady(Company* company, int semId);
 
+void readAndProcessMessages(Company *company);
+/*
+ * 1 - Wake up planes.
+ * 2 - Read & process plane messages.
+ */
 void companyStart(Company* company) {
 	int planesTurnSemId = semaphore_create(SEM_PLANE_KEY, company->planeCount, 0600);
 	int companyTurnSemId = semaphore_create(SEM_COMPANY_KEY, 1, 0600);
@@ -17,24 +22,34 @@ void companyStart(Company* company) {
 	for (int i = 0; i < 10; i++) {
 		wakeUpPlanes(company, planesTurnSemId);
 		waitUntilPlanesReady(company, companyTurnSemId);
-		// updatePlanesState(company);
+		readAndProcessMessages(company);
 	}
-	/*
-	FakeIPC fake_ipc;
-	fake_ipc.counter = 0;
-	fake_ipc.bufferSize = 1000;
-	fake_ipc.buffer = malloc(sizeof(int) * fake_ipc.bufferSize);
-	pthread_mutex_init(&fake_ipc.mutexCounter, NULL);
-	pthread_mutex_init(&fake_ipc.finished_thinking_mutex, NULL);
-	pthread_t idHilo;
-	pthread_create(&idHilo, NULL, planeStart, &fake_ipc);
-	while (1) {
-		pthread_mutex_lock(&fake_ipc.mutexCounter);
-		for (i = 0; i < fake_ipc.bufferSize; i++)
-			fake_ipc.buffer[i] = fake_ipc.counter;
-		pthread_mutex_unlock(&fake_ipc.mutexCounter);
-		fake_ipc.counter++;
-	}*/
+    exit(0);
+}
+
+void wakeUpPlanes(Company* company, int semId) {
+	printf("planes wake up!\n\n");
+	for(int i = 0; i < company->planeCount; i++) {
+		semaphore_increment(semId, i);
+	}
+}
+
+void waitUntilPlanesReady(Company* company, int semId) {
+	for(int i = 0; i < company->planeCount; i++) {
+		semaphore_decrement(semId, 0);
+	}
+	printf("Waiting done!...\n\n");
+}
+
+void readAndProcessMessages(Company *company) {
+	IpcPackage package;
+	package.id = 1;
+	for (int i = 0; i < company->planeCount; i++) {
+		//if (ipcIsReady(i, WRITE) == TRUE) {
+			//strcpy(package.data, "Message to plane :D\n");
+			//sendData(i, package);
+		//}
+	}
 	/*int i;
 	ipcPackage package;
 	package.id = 1;
@@ -47,20 +62,21 @@ void companyStart(Company* company) {
 		}
 		sleep(1);
 	}*/
-    exit(0);
 }
 
-void wakeUpPlanes(Company* company, int semId) {
-	printf("planes wake up!\n");
-	for(int i = 0; i < company->planeCount; i++) {
-		semaphore_increment(semId, i);
-	}
-}
-
-void waitUntilPlanesReady(Company* company, int semId) {
-	for(int i = 0; i < company->planeCount; i++) {
-		semaphore_decrement(semId, 0);
-	}
-	printf("Waiting done!...\n");
-}
-
+/*
+FakeIPC fake_ipc;
+fake_ipc.counter = 0;
+fake_ipc.bufferSize = 1000;
+fake_ipc.buffer = malloc(sizeof(int) * fake_ipc.bufferSize);
+pthread_mutex_init(&fake_ipc.mutexCounter, NULL);
+pthread_mutex_init(&fake_ipc.finished_thinking_mutex, NULL);
+pthread_t idHilo;
+pthread_create(&idHilo, NULL, planeStart, &fake_ipc);
+while (1) {
+	pthread_mutex_lock(&fake_ipc.mutexCounter);
+	for (i = 0; i < fake_ipc.bufferSize; i++)
+		fake_ipc.buffer[i] = fake_ipc.counter;
+	pthread_mutex_unlock(&fake_ipc.mutexCounter);
+	fake_ipc.counter++;
+}*/
