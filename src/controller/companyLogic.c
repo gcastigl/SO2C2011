6f3a5b4b15a8662da8c89465597e412ab053e6e7
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-28 16:23:46 -0300

    move dlal the company logic from company.c to companyLogic.c

---
+++ src/controller/companyLogic.c
@@ -0,0 +1,66 @@
+#include "companyLogic.h"
+
+void wakeUpPlanes(Company* company, int semId);
+
+void waitUntilPlanesReady(Company* company, int semId);
+
+void companyStart(Company* company) {
+	int planesTurnSemId = semaphore_create(SEM_PLANE_KEY, company->planeCount, 0600);
+	int companyTurnSemId = semaphore_create(SEM_COMPANY_KEY, 1, 0600);
+	if (planesTurnSemId <= 0 || companyTurnSemId <= 0) {
+		fatal("Error creating semaphore");
+	}
+	for(int i = 0; i < company->planeCount; i++) {
+		semctl(planesTurnSemId, i, SETVAL, 0);
+		pthread_create(&(company->plane[i].thread), NULL, planeStart, company->plane + i);
+	}
+	for (int i = 0; i < 10; i++) {
+		wakeUpPlanes(company, planesTurnSemId);
+		waitUntilPlanesReady(company, companyTurnSemId);
+		// updatePlanesState(company);
+	}
+	/*
+	FakeIPC fake_ipc;
+	fake_ipc.counter = 0;
+	fake_ipc.bufferSize = 1000;
+	fake_ipc.buffer = malloc(sizeof(int) * fake_ipc.bufferSize);
+	pthread_mutex_init(&fake_ipc.mutexCounter, NULL);
+	pthread_mutex_init(&fake_ipc.finished_thinking_mutex, NULL);
+	pthread_t idHilo;
+	pthread_create(&idHilo, NULL, planeStart, &fake_ipc);
+	while (1) {
+		pthread_mutex_lock(&fake_ipc.mutexCounter);
+		for (i = 0; i < fake_ipc.bufferSize; i++)
+			fake_ipc.buffer[i] = fake_ipc.counter;
+		pthread_mutex_unlock(&fake_ipc.mutexCounter);
+		fake_ipc.counter++;
+	}*/
+	/*int i;
+	ipcPackage package;
+	package.id = 1;
+	while (waitpid(-1, NULL, WNOHANG) != -1) {
+		for (i = 0; i < Company->planeCount; i++) {
+			if (ipcIsReady(i, WRITE) == TRUE) {
+				strcpy(package.data, "Message to plane :D\n");
+				sendData(i, package);
+			}
+		}
+		sleep(1);
+	}*/
+    exit(0);
+}
+
+void wakeUpPlanes(Company* company, int semId) {
+	printf("planes wake up!\n");
+	for(int i = 0; i < company->planeCount; i++) {
+		semaphore_increment(semId, i);
+	}
+}
+
+void waitUntilPlanesReady(Company* company, int semId) {
+	for(int i = 0; i < company->planeCount; i++) {
+		semaphore_decrement(semId, 0);
+	}
+	printf("Waiting done!...\n");
+}
+
---
+++ src/controller/companyLogic.h
@@ -0,0 +1,15 @@
+#ifndef COMPANYLOGIC_H_
+#define COMPANYLOGIC_H_
+
+#include "../../include/common.h"
+#include "../../include/map.h"
+#include "../../include/plane.h"
+#include <pthread.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void companyStart(Company* company);
+
+
+#endif
