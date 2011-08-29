417d5d3c12e91a83888ee51debfaa9d43ff176da
Gonzalo Castiglione <gcastigl@gmail.com>
2011-08-29 18:32:45 -0300

    solved merge problems

--- src/main.c
+++ src/main.c
@@ -3,22 +3,15 @@
 
 int main() {
     initSignalHandler();
-    int numAirlines = 1;
-    int i;
     parseMap("resources/loads/ciudades.txt");
     map_addCompany(parseCompany("resources/loads/empresa.txt"));
-    map;
-    for (i = 0; i < numAirlines; i++) {
+//    for (int i = 0; i < numAirlines; i++) {
         switch(fork()) {
             case 0:
                 initChildSignalHandler();
                 log_debug("Creating airline...\n");
                 log_error("Bleh\n");
-                Company* company = newCompany("pepe", 3);
-                for( int i = 0; i < 3; i++) {
-                	addPlane(company, *newPlane(i, 0));
-                }
-                companyStart(company);
+                companyStart(map->company[0]);
                 break;
             case ERROR:
                 fatal("Fork Error");
@@ -27,7 +20,7 @@ int main() {
                 wait(NULL);
                 break;
         }
-    }
+ //   }
     printf("Simulation Done!\n");
 	return 0;
 }
