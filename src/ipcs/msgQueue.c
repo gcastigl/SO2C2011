dd8880d51c16b04198fe3797a299973a9ac6b738
Gonzalo <gonzalo@gonzalo.(none)>
2011-08-29 22:35:06 -0300

    implemented new communicator method and fixed problem when writing and reading messages

--- src/ipcs/msgQueue.c
+++ src/ipcs/msgQueue.c
@@ -1,33 +1,37 @@
 #include "communicator.h"
 
 int ipc_init(int key, int flags) {
-	key_t ipdId;
-	ipdId = ftok(DEFAULT_FOLDER, key);
-	if (ipdId == (key_t)-1) {
+	key_t ipcId;
+	ipcId = ftok("/tmp", key);
+	if (ipcId == (key_t)-1) {
 		return -1;
 	}
-	int id = msgget(ipdId, flags | IPC_CREAT);
-	if (id == -1) {
+	int id = msgget(key, flags | IPC_CREAT);
+	if (id < 0) {
 		return -1;
 	}
 	return id;
 }
 
-int ipc_write(int ipcId, IpcPackage* msg) {
-	msg->id = 0;
-	return msgsnd(ipcId, (struct msgbuf *) msg,
-		sizeof(msg->numericDataType) + sizeof(msg->data), IPC_NOWAIT);
+int ipc_get(int key) {
+	return ipc_init(key, 0666);
 }
 
-// - Identificador del tipo de mensaje que queremos recibir. En este caso
-// se quiere un mensaje de tipo 2. Si ponemos tipo 1, se extrae el mensaje
-// que se acaba de enviar en la llamada anterior a msgsnd().
+int ipc_write(int ipcId, IpcPackage* msg) {
+	msg->id = 1;
+	int result = msgsnd(ipcId, (void *) msg, sizeof(msg->data), IPC_NOWAIT);
+	if (result == -1) {
+		perror("msgsnd failed");
+	}
+	return result;
+}
+
 IpcPackage* ipc_read(int ipcId) {
 	IpcPackage* msg = malloc(sizeof(IpcPackage));
-	int result = msgrcv(ipcId, (struct msgbuf *) msg,
-		sizeof(msg->numericDataType) + sizeof(msg->data),
-		2, IPC_NOWAIT);
-	if (result <= 0) {
+	int result = msgrcv(ipcId, msg, sizeof(msg->data), 1, IPC_NOWAIT);
+	if (result < 0) {
+		free(msg);
+		perror("msgrcv failed");
 		return NULL;
 	}
 	return msg;
