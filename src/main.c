f25098d901a392642d0c3b1e5d0e1ea206c3766b
gcastigl <gcastigl@gmail.com>
2011-09-03 13:03:04 -0300

    updated main

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main() {
     //initSignalHandler();
     parseMap("resources/loads/ciudades.txt");
-    map_addCompany(parseCompany("resources/loads/empresa.txt", 123456));
+    map_addCompany(parseCompany("resources/loads/empresa.txt", MIN_COMPANY_ID + 0));
     companyStart(map->company[0]);
 /*    for (int i = 0; i < numAirlines; i++) {
         switch(fork()) {
