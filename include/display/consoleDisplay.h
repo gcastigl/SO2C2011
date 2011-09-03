2459e7a2314cc9183b98601ae17ea7358b8a93f8
gcastigl <gcastigl@gmail.com>
2011-09-03 16:37:02 -0300

    created a consoleDisplay file

---
+++ include/display/consoleDisplay.h
@@ -0,0 +1,9 @@
+#ifndef CONSOLEDISPLAY_H_
+#define CONSOLEDISPLAY_H_
+
+#include "../common.h"
+#include "../map.h"
+
+void displaySimulation();
+
+#endif
---
+++ src/display/consoleDisplay.c
@@ -0,0 +1,39 @@
+#include "display/consoleDisplay.h"
+
+void displayCompanies();
+
+void displayCompany(Company* company);
+
+void displayPlane(Plane *plane);
+
+void displaySimulation() {
+	void initSignalHandler();
+	while(1) {
+		printf("\n-----------------------------------------------\n");
+		displayCompanies();
+		sleep(1);
+	}
+}
+
+void displayCompanies() {
+	for(int i = 0; i < map->companyCount; i++) {
+		displayCompany(map->company[i]);
+	}
+}
+
+void displayCompany(Company *company) {
+	printf("\n----- Company %d - %20s ---------\n", company->id, company->name);
+	for(int i = 0; i < company->planeCount; i++) {
+		displayPlane(company->plane[i]);
+	}
+	printf("\n");
+}
+
+
+void displayPlane(Plane *plane) {
+	printf("\n\t------- Plane %d ----------\n", plane->id);
+	printf("\tFrom: %d\n", plane->cityIdFrom);
+	printf("\tTo: %d\n", plane->cityIdTo);
+	printf("\tDistance left: %d\n", plane->distanceToDestination);
+	printf("\n");
+}
