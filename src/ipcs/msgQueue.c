b5989e8fb84d43c4182da39adaea34a99c3bc60e
gcastigl <gcastigl@gmail.com>
2011-09-04 20:35:43 -0300

    updated MsgQueue

--- src/ipcs/msgQueue.c
+++ src/ipcs/msgQueue.c
@@ -1,33 +1,60 @@
 #include "communicator.h"
 
-#define MSG_SIZE (sizeof(IpcPackage) - sizeof(long int))
+typedef struct {
+	long fromId;
+	char data[DATA_SIZE];
+} MsgQueuePackage;
 
-int ipc_init(int key, int flags) {
+#define MSG_SIZE (sizeof(MsgQueuePackage) - sizeof(long int))
+
+int ipc_get(int id);
+
+MsgQueuePackage *newMsgQueuePackage(int id, char* data);
+
+void ipc_init(int id) {
+	// TODO: ???
+}
+
+int ipc_get(int key) {
 	key_t ipcId;
 	ipcId = ftok(TMP_FOLDER, key);
 	if (ipcId == (key_t)-1) {
+		fatal("Error writing msg - ftok");
 		return -1;
 	}
-	int id = msgget(key, flags);
+	int id = msgget(ipcId, IPC_CREAT | FLAGS);
 	if (id < 0) {
+		fatal("Error writing msg - msgget");
 		return -1;
 	}
 	return id;
 }
 
-int ipc_get(int key) {
-	return ipc_init(key, IPC_CREAT | 0666);
+int ipc_write(int myId, int toId, char *data) {
+	int fromIpcId = ipc_get(myId);
+	MsgQueuePackage *msg = newMsgQueuePackage(myId, data);
+	return msgsnd(fromIpcId, (void*) msg, MSG_SIZE, IPC_NOWAIT);
 }
 
-int ipc_write(int ipcId, IpcPackage* msg) {
-	return msgsnd(ipcId, msg, MSG_SIZE, IPC_NOWAIT);
-}
-
-int ipc_read(int ipcId, int fromId, IpcPackage* msg) {
-	int result = msgrcv(ipcId, (void *) msg, MSG_SIZE, fromId, IPC_NOWAIT);
+int ipc_read(int myId, int fromId, char *data) {
+	int toIdIpc = ipc_get(myId);
+	MsgQueuePackage msg;
+	int result = msgrcv(toIdIpc, (void *)&msg, MSG_SIZE, fromId, IPC_NOWAIT);
+	if (result == -1) {
+		return -1;
+	}
+	strcpy(data, msg.data);
 	return result;
 }
 
-void ipc_close(int ipcId) {
-	msgctl(ipcId, IPC_RMID, (struct msqid_ds *) NULL);
+int ipc_close(int id){
+	int ipcId = ipc_get(id);
+	return msgctl(ipcId, IPC_RMID, (struct msqid_ds *) NULL);
+}
+
+MsgQueuePackage *newMsgQueuePackage(int id, char* data) {
+	MsgQueuePackage* msg = malloc(sizeof(MsgQueuePackage));
+	msg->fromId = id;
+	strcpy(msg->data, data);
+	return msg;
 }
