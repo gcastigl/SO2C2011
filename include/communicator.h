b1079f5b13f93f45c1d97bad5d432859cf441cf1
gcastigl <gcastigl@gmail.com>
2011-09-04 20:34:11 -0300

    updates communicator iinterface

--- include/communicator.h
+++ include/communicator.h
@@ -2,36 +2,20 @@
 #define MYIPC_H_
 
 #include "ipc.h"
+#include "common.h"
+#include "map.h"
 #include <sys/msg.h>
 #include <errno.h>
 
-typedef enum {
-    CREATE,
-	DESTROY,
-	MESSAGE
-} packageType;
+#define FLAGS	0666
+#define DATA_SIZE 128
 
-/*
- * Estructura para los mensajes que se quieren enviar y/o recibir. Deben llevar
- * obligatoriamente como primer campo un long para indicar un identificador
- * del mensaje.
- * Los siguientes campos son la información que se quiera transmitir en el
- * mensaje.
- */
-typedef struct {
-	long addressee;		/* addressee, must be > 0 */
-	long sender;
-	char data[DATA_SIZE];
-} IpcPackage;
+void ipc_init(int id);
 
-int ipc_init(int key, int flags);
+int ipc_write(int myId, int toId, char *msg);
 
-int ipc_get(int key);
+int ipc_read(int myId, int fromId, char *msg);
 
-int ipc_write(int ipcId, IpcPackage* msg);
-
-int ipc_read(int ipcId, int fromId, IpcPackage* msg);
-
-void ipc_close(int ipcId);
+int ipc_close(int id);
 
 #endif
