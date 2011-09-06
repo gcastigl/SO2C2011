a3cd708432fdf79cfcbedc08ede72be0660845e8
gcastigl <gcastigl@gmail.com>
2011-09-05 22:02:07 -0300

    possible main structure

--- src/main.c
+++ src/main.c
@@ -6,39 +6,53 @@
 #include <time.h>
 #include <sys/wait.h>
 
-void startSimulation();
 void initEnvironment();
-void startMapAndDisplayProcess();
-void main_endSimulation();
+void initializeServer();
+void initializeCompanies();
+void endSimulation();
+
 static Map *map;
 static int processCount;
+static Server server;
 
+/*
+ * 1 - Initialize Environment
+ * 2 - Initialize Server
+ * 3 - Initialize Companies
+ * 4 - StartServer
+ * 5 - EndSimulation
+ */
 int main() {
+	log_debug("Starting simulation...\n");
     initEnvironment();
-    startMapAndDisplayProcess();
-    startSimulation();
+    initializeServer();
+    initializeCompanies();
+    server_start();
+    endSimulation();
     printf("\n\nSimulation Done!\n\n");
 	return 0;
 }
 
+
 void initEnvironment() {
-    log_debug("Starting simulation...\n");
-    map = parseMap("resources/loads/ciudades.txt");
-    map_addCompany(map, parseCompany(map, "resources/loads/empresa.txt", 1));
-    processCount = map->companyCount;
     signal_createHandlerThread(TRUE);
+    map = parseMap("resources/loads/ciudades.txt");
     childPid = malloc(sizeof(int) * (processCount));
-    return;
+}
+//TODO: initializer server
+void initializeServer() {
+	server = NULL;
+	// server_addCompany(map, parseCompany(map, "resources/loads/empresa.txt", 1));
+	processCount = server->companyCount;
 }
 
-void startSimulation() {
+void initializeCompanies() {
     pid_t pId;
-    // Initialize companies
-    for (int i = 0; i < map->companyCount; i++) {
+    for (int i = 0; i < server->companyCount; i++) {
         switch((pId = fork())) {
             case 0:
                 signal_createHandlerThread(FALSE);
-                companyStart(map->company[i]);
+                companyStart(map, server->company[i]);
                 break;
             case ERROR:
                 fatal("Fork Error");
@@ -48,21 +62,14 @@ void startSimulation() {
                 break;
         }
     }
-    for (int i = 0; i < map->companyCount; ++i) {
-        wait(NULL);
-    }
 }
 
-// Also acts as a map information hub
-void startMapAndDisplayProcess() {
-	display_start(map);
-	map_start(map);
-}
-
-void main_endSimulation() {
+void endSimulation() {
     for (int i = 0; i < processCount; i++) {
         kill(childPid[i], SIGUSR1);
     }
+    log_debug("\n\nSimulation Done!\n\n");
     logger_end();
     exit(0);
 }
+
