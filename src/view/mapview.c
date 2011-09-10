e91dbe06af9f04ebd2c8720ca361c6648e28f0b8
gcastigl <gcastigl@gmail.com>
2011-09-10 18:59:53 -0300

    the UI now clears the screen b4 showing a new turn

--- src/view/mapview.c
+++ src/view/mapview.c
@@ -7,12 +7,13 @@ void view_start() {
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
 	init_pair(5, COLOR_RED, COLOR_BLACK);
 	init_pair(9, COLOR_CYAN, COLOR_BLACK);
-	char *title = "Pharma-Plane Simulation 1.0";
-	mvprintw(1, (COLS-strlen(title))/2, title);
 	refresh();
 }
 
 void printTurn(Server *server) {
+	wclear(stdscr);
+	char *title = "Pharma-Plane Simulation 1.0";
+	mvprintw(1, (COLS-strlen(title))/2, title);
 	attron(COLOR_PAIR(5));
 	mvprintw(1, 1, "TURN: %d", server->turn);
 	attroff(COLOR_PAIR(5));
